148971ac91975a1b8d6a249b319bf751adb3fdb2
Jan Cizmar <cizmar@chlupac.com>
2023-10-17 16:59:17 +0200

    fix: Migrate batch, dockerfile

--- backend/data/src/main/kotlin/io/tolgee/batch/BatchJobService.kt
+++ backend/data/src/main/kotlin/io/tolgee/batch/BatchJobService.kt
@@ -31,7 +31,6 @@ import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionalEventListener
-import java.math.BigInteger
 import java.util.*
 
 @Service
